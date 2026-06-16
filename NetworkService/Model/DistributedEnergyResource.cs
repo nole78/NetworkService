8ce4d61e9e2aae8b2cf5aec76b418b8134129606
David Novaković <daki.nole@gmail.com>
2026-06-16 14:40:26 +0200

    Add project files.

---
+++ NetworkService/Model/DistributedEnergyResource.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkService.Model
+{
+    public class DistributedEnergyResource : INotifyPropertyChanged
+    {
+        private int _id;
+        private string _name;
+        private EnergyResourceType _type;
+        private double _value;
+
+        #region Properties
+        public int Id
+        {
+            get => _id;
+            set
+            {
+                if (value != _id)
+                {
+                    _id = value;
+                    OnPropertyChanged(nameof(Id));
+                }
+            }
+        }
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (value != _name)
+                {
+                    _name = value;
+                    OnPropertyChanged(nameof(Name));
+                }
+            }
+        }
+        public EnergyResourceType Type
+        {
+            get => _type;
+            set
+            {
+                if (value != _type)
+                {
+                    _type = value;
+                    OnPropertyChanged(nameof(Type));
+                }
+            }
+        }
+        public double Value
+        {
+            get => _value;
+            set
+            {
+                if (value != _value)
+                {
+                    _value = value;
+                    OnPropertyChanged(nameof(Value));
+                }
+            }
+        }
+
+        #endregion
+
+        public DistributedEnergyResource() { }
+        public DistributedEnergyResource(int id, string name, EnergyResourceType type, double value)
+        {
+            Id = id;
+            Name = name;
+            Type = type;
+            Value = value;
+        }
+
+        #region PropertyChanged Implementation
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+    }
+}
---
+++ NetworkService/Model/EnergyResourceType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkService.Model
+{
+    public class EnergyResourceType
+    {
+        public string Name { get; set; }
+        public string ImagePath { get; set; }
+
+        public EnergyResourceType(string name, string imagePath)
+        {
+            Name = name;
+            ImagePath = imagePath;
+        }
+
+        public EnergyResourceType() {}
+    }
+}
