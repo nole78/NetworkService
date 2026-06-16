fe3ed26c1cad1b214f330b384e957ef8f49c2898
David Novaković <daki.nole@gmail.com>
2026-06-16 18:08:07 +0200

    Added TerminalLine model for TerminalView

---
+++ NetworkService/Model/TerminalLine.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkService.Model
+{
+    public enum LineType
+    {
+        Command,
+        Response,
+        Error,
+        Success
+    }
+    public class TerminalLine : INotifyPropertyChanged
+    {
+        private string _text;
+        private LineType _type;
+
+        #region Properties
+
+        public string Text
+        {
+            get => _text;
+            set
+            {
+                if (_text != value)
+                {
+                    _text = value;
+                    OnPropertyChanged(nameof(Text));
+                }
+            }
+        }
+        public LineType Type
+        {
+            get => _type;
+            set
+            {
+                if (_type != value)
+                {
+                    _type = value;
+                    OnPropertyChanged(nameof(Type));
+                }
+            }
+        }
+
+        #endregion
+
+        public TerminalLine() { }
+        public TerminalLine(string text, LineType  type)
+        {
+            Text = text;
+            Type = type;
+        }
+
+        #region INotifyPropertyChanged implementation
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+    }
+}
