290a1ebc7ce5cd3cf68f735e9cf0db35faf0faeb
David Novaković <daki.nole@gmail.com>
2026-06-20 22:33:46 +0200

    Implemented measurment processing service

---
+++ NetworkService/Services/MeasurementProcessingService.cs
@@ -0,0 +1,50 @@
+﻿using NetworkService.Persistance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace NetworkService.Services
+{
+    public class MeasurementProcessingService
+    {
+        private readonly LoggerService _loggerService;
+
+        public static event Action<int, double> OnMeasurementProcessed;
+
+        public MeasurementProcessingService(LoggerService loggerService)
+        {
+            _loggerService = loggerService;
+        }
+
+        public void ProcessMeasurement(string incoming)
+        {
+            try
+            {
+                var parts = incoming.Trim().Split(':');
+                int idx = int.Parse(parts[0].Split('_')[1]);
+                double value = double.Parse(parts[1]);
+
+                Application.Current.Dispatcher.Invoke(() => {
+                    if (AppDatabase.Resources.Count > idx)
+                    {
+                        var resource = AppDatabase.Resources[idx];
+                        resource.Value = value;
+                    }
+                });
+
+                var date = DateTime.Now;
+                string logLine = $"{date.ToShortDateString()}, {date.ToShortTimeString()}: {idx}, {value}";
+                _loggerService.LogLine(logLine);
+
+                OnMeasurementProcessed?.Invoke(idx, value);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Error processing measurment: {ex.Message}");
+            }
+        }
+    }
+}
