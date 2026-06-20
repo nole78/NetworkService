2d0fcf0a1e05108b358c5987d476bcec2394c0bc
David Novaković <daki.nole@gmail.com>
2026-06-20 22:03:53 +0200

    Added Measurment model

---
+++ NetworkService/Model/Measurment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkService.Model
+{
+    public class Measurment
+    {
+        public string Time { get; set; }
+        public double Value { get; set; }
+        public Measurment() { }
+        public Measurment(string time, double value)
+        {
+            Time = time;
+            Value = value;
+        }
+    }
+}
