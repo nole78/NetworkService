c63c08d5e32feff2a03fd7ba485fb0b85966552c
David Novaković <daki.nole@gmail.com>
2026-06-20 16:31:58 +0200

    Added value converter for line position

---
+++ NetworkService/Converters/AddValueConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace NetworkService.Converters
+{
+    public class AddValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is double currentPosition && parameter != null)
+            {
+                if(double.TryParse(parameter.ToString(),out double offset))
+                {
+                    return currentPosition + offset;
+                }
+            }
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double currentPosition && parameter != null)
+            {
+                if (double.TryParse(parameter.ToString(), out double offset))
+                {
+                    return currentPosition - offset;
+                }
+            }
+
+            return value;
+        }
+    }
+}
