e207367437f2e81533a0efd3c45a5760d56337de
David Novaković <daki.nole@gmail.com>
2026-06-21 21:45:32 +0200

    Added PlaceResource action for drag and drop

---
+++ NetworkService/Model/Actions/PlaceResourceAction.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkService.Model.Actions
+{
+    public class PlaceResourceAction : IUndoableAction
+    {
+        private readonly DistributedEnergyResource _resource;
+        private readonly DistributedEnergyResource[] _slots;
+        private readonly int _targetSlotIdx;
+
+        public PlaceResourceAction(DistributedEnergyResource resource, DistributedEnergyResource[] slots, int targetSlotIdx)
+        {
+            _resource = resource;
+            _slots = slots;
+            _targetSlotIdx = targetSlotIdx;
+        }
+
+        public bool Do()
+        {
+            if (_slots[_targetSlotIdx] == null) return false;
+
+            _slots[_targetSlotIdx] = _resource;
+            return true;
+        }
+
+        public void Undo()
+        {
+            if(_slots[_targetSlotIdx] == null) return;
+
+            if (_slots[_targetSlotIdx].Id == _resource.Id)
+            {
+                _slots[_targetSlotIdx] = null;
+            }
+        }
+    }
+}
