1ac37128f67a7ae87952d689dd88490a07f81dd7
David Novaković <daki.nole@gmail.com>
2026-06-22 21:26:10 +0200

    Added LineConnection model to be able to save resource relations

---
+++ NetworkService/Model/LineConnection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkService.Model
+{
+    public class LineConnection
+    {
+        public int FromSlot { get; set; }
+        public int ToSlot { get; set; }
+
+        public LineConnection(){ }
+
+        public LineConnection(int fromSlot, int toSlot)
+        {
+            FromSlot = fromSlot;
+            ToSlot = toSlot;
+        }
+    }
+}
