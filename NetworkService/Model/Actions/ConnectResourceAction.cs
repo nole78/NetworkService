0eff888fb0222a69cd7c4170144a1dd3e691c286
David Novaković <daki.nole@gmail.com>
2026-06-22 21:26:27 +0200

    Implemented ConnectResource action with its do and undo feature

---
+++ NetworkService/Model/Actions/ConnectResourceAction.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkService.Model.Actions
+{
+    public class ConnectResourceAction : IUndoableAction
+    {
+        private readonly int _firstSlotIdx;
+        private readonly int _secondSlotIdx;
+        private readonly ObservableCollection<LineConnection> _connections;
+        private readonly DistributedEnergyResource[] _slots;
+
+        public ConnectResourceAction(int firstSlotIdx, int secondSlotIdx, ObservableCollection<LineConnection> connections, DistributedEnergyResource[] slots)
+        {
+            _firstSlotIdx = firstSlotIdx;
+            _secondSlotIdx = secondSlotIdx;
+            _connections = connections;
+            _slots = slots;
+        }
+
+        public bool Do()
+        {
+            if (_slots.Length < _firstSlotIdx || _slots.Length < _secondSlotIdx)
+                return false;
+
+            if (_slots[_firstSlotIdx] == null || _slots[_secondSlotIdx] == null)
+                return false;
+
+            _connections.Add(new LineConnection(_firstSlotIdx, _secondSlotIdx));
+            return true;
+        }
+
+        public void Undo()
+        {
+            var toDelete = _connections.FirstOrDefault(c => c.ToSlot == _secondSlotIdx && c.FromSlot == _firstSlotIdx);
+            if (toDelete == null) 
+                return;
+
+            _connections.Remove(toDelete);
+        }
+    }
+}
